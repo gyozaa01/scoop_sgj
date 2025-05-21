7cb28f7e6ac37450ea86fcadd55dedd3a1ee3333
gyozaa01 <olivia110321@gmail.com>
2025-05-21 13:45:10 +0900

    feat: 초기 환경 설정

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-blue-500">
+        스쿱 프론트엔드 개발자 지원자 서교진
+      </h1>
+    </div>
+  );
+}
+
+export default App;
